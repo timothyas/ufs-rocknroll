0892bcec32220c544dfc2ffee4a7e71ae2d17a81
timothyas <timsmith204@utexas.edu>
2023-05-12 22:34:21 +0000

    working... take 'products' datasets, do some basic stuff and store them

--- rocknroll/io.py
+++ rocknroll/io.py
@@ -5,17 +5,17 @@ import numpy as np
 from zarr import NestedDirectoryStore
 import xarray as xr
 
-
 from datetime import datetime, timedelta
+from cftime import DatetimeJulian
 
 """Decisions to be made:
 
     - Save as float32?
+    - snapshots not averages
     - Use cftime or numpy.datetime64?
     - Make consistent dimension names across files
     - fields to be saved:
         -> FV3 missing surface
-        -> WTF is going on with CICE fields
 """
 
 
@@ -27,18 +27,47 @@ class EnsembleDataset():
     nc_glob     = None
     fhr         = 3
     time_format = "%Y-%m-%dT%H-%M-%S"
+    zarr_name   = None
 
     @property
-    def products_path(self):
+    def products_dir(self):
         return join(os.getenv("SCRATCH"), "work", self.experiment, "products", "forecast")
+
+    @property
+    def coords_dir(self):
+        return join(os.getenv("SCRATCH"), "work", self.experiment, "products", "coordinates")
+
+    @property
+    def coords_path(self):
+        return join(self.coords_dir, self.zarr_name)
 
 
     def __init__(self, experiment):
         self.experiment = experiment
 
+
     def get_forecast_time(self, cycle):
         t0 = datetime.strptime(cycle, self.time_format)
         return t0 + timedelta(hours=self.fhr)
+
+
+    def get_forecast_time_vector(self, cycle):
+        """All possible times for a dataset that has multiple time steps"""
+        t0 = datetime.strptime(cycle, self.time_format)
+        t = [np.datetime64(t0 + timedelta(hours=x)) for x in [3, 6, 9, 12]]
+        return np.array(t)
+
+
+    def get_ensemble_path(self, cycle):
+        return join(self.products_dir, "ensemble", cycle, self.zarr_name)
+
+
+    def get_mean_path(self, cycle):
+        return join(self.products_dir, "ensemble-mean", cycle, self.zarr_name)
+
+
+    def get_std_path(self, cycle):
+        return join(self.products_dir, "ensemble-std", cycle, self.zarr_name)
 
 
     def get_ncfilename(self, cycle):
@@ -70,7 +99,7 @@ class EnsembleDataset():
         kw.update(kwargs)
 
         xds = xr.open_mfdataset(
-                join(self.products_path, cycle, "mem*", self.get_ncfilename(cycle)),
+                join(self.products_dir, cycle, "mem*", self.get_ncfilename(cycle)),
                 **kw)
 
         xds.attrs.update({
@@ -82,20 +111,66 @@ class EnsembleDataset():
         return xds.squeeze()
 
 
-    def store_ensemble_dataset(self, xds):
-
-        for key in xds.data_vars:
-            xds[key] = xds[key].astype(np.float32)
+    def chunk(self, xds):
 
         chunks = self.chunks_out.copy()
         for key in self.chunks_out.keys():
             if key not in xds.dims:
                 chunks.pop(key)
 
-        xds = xds.chunk(chunks)
-        path = join(self.products_path, "ensemble", xds.cycle, self.zarr_name)
+        return xds.chunk(chunks)
+
+
+    def store_coordinate_dataset(self, cds):
+
+        try:
+            assert len(cds.data_vars) == 0
+        except AssertionError:
+            msg = f"EnsembleDataset.store_coordinate_dataset: we should not have any data variables in this dataset, but we found some."
+            msg += f"\n{cds.data_vars}"
+            raise AttributeError(msg)
+
+        # these don't need to be chunked, coordinates are opened in memory
+        store = NestedDirectoryStore(path=self.coords_path)
+        cds.to_zarr(store, mode="w")
+        print(f"Stored coordinate dataset at {self.coords_path}")
+
+
+    def store_ensemble_dataset(self, xds):
+
+        xds = self.chunk(xds)
+
+        path = self.get_ensemble_path(cycle=xds.cycle)
         store = NestedDirectoryStore(path=path)
         xds.to_zarr(store, mode="w")
+        print(f"Stored ensemble dataset at {path}")
+
+
+    def store_reduced_ensemble_dataset(self, cycle, reduction="mean"):
+        """Reduction is applied across 'member' axis"""
+
+        try:
+            assert reduction in ("mean", "std")
+        except:
+            raise NotImplementedError("only mean and std coded up")
+
+        epath = self.get_ensemble_path(cycle=cycle)
+        xds = xr.open_zarr(epath)
+
+        with xr.set_options(keep_attrs=True):
+            xds = xds.mean("member") if reduction == "mean" else \
+                  xds.std("member")
+
+        # add to meta data to let user know this happened
+        for key in xds.data_vars:
+            cell_methods = xds[key].attrs.get("cell_methods", "")
+            xds[key].attrs["cell_methods"] = f"{cell_methods} member: {reduction}"
+
+        path = self.get_mean_path(cycle=cycle) if reduction == "mean" else \
+               self.get_std_path(cycle=cycle)
+        store = NestedDirectoryStore(path=path)
+        xds.to_zarr(store, mode="w")
+        print(f"Stored ensemble {reduction} dataset at {path}")
 
 
 class MOMDataset(EnsembleDataset):
@@ -104,49 +179,59 @@ class MOMDataset(EnsembleDataset):
     chunks_in   = {
             "Layer"     : 5,
             "Interface" : 5,
-            "lath"      : -1,
-            "lonh"      : -1,
-            "latq"      : -1,
-            "lonq"      : -1,
+            "yh"        : -1,
+            "xh"        : -1,
+            "yq"        : -1,
+            "xq"        : -1,
             }
 
     chunks_out  = {
             "member"    : 50,
             "layer"     : 5,
             "interface" : 5,
-            "lath"      : 30,
-            "lonh"      : 30,
-            "latq"      : 30,
-            "lonq"      : 30,
+            "yh"        : 30,
+            "xh"        : 30,
+            "yq"        : 30,
+            "xq"        : 30,
             }
 
 
     def get_ncfilename(self, cycle):
-        tf = self.get_forecast_time(cycle)
-        tfstr = tf.strftime(self.time_format)
-        return f"mom.{tfstr}.nc"
+        return "ocn.nc"
 
 
     def open_ensemble_dataset(self, cycle, **kwargs):
         xds = super().open_ensemble_dataset(cycle, **kwargs)
 
-        # Recreate time to be... sensible, rename
+        # rename time to use numpy.datetime64, but keep cftime
+        # also make a "forecast_time" which is a timedelta
+        xds = xds.rename({"time": "cftime"})
+        t0 = np.datetime64(datetime.strptime(cycle, self.time_format))
+        time = self.get_forecast_time_vector(cycle)
         xds["time"] = xr.DataArray(
-                np.datetime64(self.get_forecast_time(cycle)),
-                coords=xds["Time"].coords,
-                dims=xds["Time"].dims,
+                time,
+                coords=xds["cftime"].coords,
+                dims=xds["cftime"].dims,
                 attrs={
                     "long_name": "time",
-                    "cartesian_axis": "T"}
+                    "axis": "T"},
                 )
-        xds = xds.set_coords("time")
-        xds = xds.drop("Time")
 
-        # lowercase coordinates please
-        for key in xds.coords:
-            xds = xds.rename({key: key.lower()})
-
+        xds["ftime"] = xr.DataArray(
+                time-t0,
+                coords=xds["cftime"].coords,
+                dims=xds["cftime"].dims,
+                attrs={
+                    "long_name": "forecast_time",
+                    "description": f"time passed since {cycle}",
+                    "axis": "T"},
+                )
+        # use forecast time to pick the single hour we want
+        # ... this convenience is why we use numpy.datetime/timedelta!
+        xds = xds.swap_dims({"cftime": "ftime"})
+        xds = xds.sel(ftime=f"{self.fhr}h")
         return xds
+
 
 class FV3Dataset(EnsembleDataset):
     zarr_name   = "fv3.zarr"
@@ -170,30 +255,57 @@ class FV3Dataset(EnsembleDataset):
     def open_ensemble_dataset(self, cycle, **kwargs):
         xds = super().open_ensemble_dataset(cycle, **kwargs)
 
-
+        # Deal with time
         xds = xds.rename({"time": "cftime"})
+        t0 = np.datetime64(datetime.strptime(cycle, self.time_format))
+        time = np.datetime64(self.get_forecast_time(cycle))
         xds["time"] = xr.DataArray(
-                np.datetime64(self.get_forecast_time(cycle)),
+                time,
                 coords=xds["cftime"].coords,
                 dims=xds["cftime"].dims,
                 attrs={
                     "long_name": "time",
-                    "cartesian_axis": "T"}
+                    "axis": "T"}
                 )
-        xds = xds.set_coords(["time", "lon", "lat"])
+        xds["ftime"] = xr.DataArray(
+                time-t0,
+                coords=xds["cftime"].coords,
+                dims=xds["cftime"].dims,
+                attrs={
+                    "long_name": "forecast_time",
+                    "description": "time passed since {cycle}",
+                    "axis": "T"},
+                )
+
+        # convert ak/bk attrs to coordinates
+        for key in ["ak", "bk"]:
+            xds[key] = xr.DataArray(
+                    xds.attrs.pop(key),
+                    coords=xds["phalf"].coords,
+                    dims=xds["phalf"].dims,
+                    )
+            xds = xds.set_coords(key)
+
+        # rename grid_yt.long_name to avoid typo
+        xds["grid_yt"].attrs["long_name"] = "T-cell latitude"
         return xds
 
 
 class CICEDataset(EnsembleDataset):
     zarr_name   = "cice.zarr"
     chunks_in   = {
-            "ncat"      : -1,
+            "nc"        : 1,
+            "nkice"     : 1,
+            "nkaer"     : 1,
+            "nksnow"    : 1,
             "nj"        : -1,
             "ni"        : -1,
             }
     chunks_out  = {
             "member"    : 50,
-            "ncat"      : 5,
+            "NCAT"      : 5,
+            "VGRDi"     : 7,
+            "VGRDa"     : 5,
             "nj"        : 30,
             "ni"        : 30,
             }
@@ -207,14 +319,55 @@ class CICEDataset(EnsembleDataset):
     def open_ensemble_dataset(self, cycle, **kwargs):
         xds = super().open_ensemble_dataset(cycle, **kwargs)
 
-        time = [np.datetime64(self.get_forecast_time(cycle))]
-        xds["time"] = xr.DataArray(
-                time,
-                coords={"time": time},
-                dims=("time",),
+        # Deal with time
+        t0 = np.datetime64(datetime.strptime(cycle, self.time_format))
+        time = np.datetime64(self.get_forecast_time(cycle))
+
+        # make cftime to be consistent with MOM and FV3
+        cftime = DatetimeJulian(
+                int(xds.time.dt.year),
+                int(xds.time.dt.month),
+                int(xds.time.dt.day),
+                int(xds.time.dt.hour),
+                int(xds.time.dt.minute),
+                int(xds.time.dt.second),
+                has_year_zero=False)
+        xds["cftime"] = xr.DataArray(
+                cftime,
+                coords=xds["time"].coords,
+                dims=xds["time"].dims,
                 attrs={
                     "long_name": "time",
-                    "cartesian_axis": "T"}
+                    "axis": "T",
+                    "calendar_type": "JULIAN"},
                 )
-        xds = xds.set_coords(["time"])
+        xds["ftime"] = xr.DataArray(
+                time-t0,
+                coords=xds["cftime"].coords,
+                dims=xds["cftime"].dims,
+                attrs={
+                    "long_name": "forecast_time",
+                    "description": "time passed since {cycle}",
+                    "axis": "T"},
+                )
+
+        # Swap meaningless logical indices for values, where it makes sense (basically, not ni,nj)
+        # Note we can only do this for time_bounds b/c ds is squeezed
+        swap = {"nc"    : "NCAT",
+                "nkaer" : "VGRDa",
+                "nkice" : "VGRDi",
+                "nksnow": "VGRDs",
+                "d2"    : "time_bounds",
+                }
+        for org, new in swap.items():
+            if new in xds and xds[new].ndim>0:
+                if "member" in xds[new].dims:
+                    xds[new] = xds[new].isel(member=0)
+                xds = xds.swap_dims({org: new})
+            if org in xds:
+                xds = xds.drop(org)
+
+        # This one gets squeezed out and is for some reason problematic?
+        if xds["VGRDs"].ndim == 0:
+            xds = xds.drop("VGRDs")
         return xds
--- scripts/store_datasets.py
+++ scripts/store_datasets.py
@@ -1,9 +1,11 @@
 """Read in forecasts, convert and store"""
 
+import os
 from os.path import join
 import yaml
 from rocknroll import MOMDataset, FV3Dataset, CICEDataset
-from zarr import NestedDirectoryStore
+
+import numpy as np
 
 
 def get_varlist(model):
@@ -12,16 +14,44 @@ def get_varlist(model):
 
     return diags[model]
 
+def store_ensemble(xds, drop_coords):
+    xds = xds.reset_coords()
+
+    # need to make coordinates dataset only one time
+    if not drop_coords:
+        coords = [x for x in allvars["coords"] if x in xds]
+        cds = xds[coords].set_coords(coords)
+        if "member" in cds:
+            cds = cds.isel(member=0).drop("member")
+        op.store_coordinate_dataset(cds)
+
+    # now data variables for all ensemble members at this cycle
+    # make various time variables as coordinates
+    data_vars = [x for x in allvars["data_vars"] if x in xds]
+    xds = xds.set_coords(["time", "cftime", "ftime"])
+    xds = xds[data_vars]
+
+    op.store_ensemble_dataset(xds)
+
 
 if __name__ == "__main__":
 
-    for name, Opener in zip(
-            ["cice", "fv3", "mom"],
-            [CICEDataset, FV3Dataset, MOMDataset]
-            ):
-        varlist = get_varlist(name)
-        mom = Opener(experiment="small-2")
-        xds = mom.open_ensemble_dataset(cycle="2015-12-01T00-00-00")
-        xds = xds[varlist]
-        mom.store_ensemble_dataset(xds)
-        print(f"stored {name}.zarr...")
+
+    cycle = "2015-12-01T00-00-00"
+    models = {
+            "fv3": FV3Dataset,
+            "mom": MOMDataset,
+            "cice": CICEDataset}
+
+    for name, Opener in models.items():
+
+        allvars = get_varlist(name)
+
+        op = Opener(experiment="small-2")
+
+        drop_coords = os.path.isdir(op.coords_path)
+        xds = op.open_ensemble_dataset(cycle=cycle)
+
+        store_ensemble(xds, drop_coords)
+        op.store_reduced_ensemble_dataset(cycle=cycle, reduction="mean")
+        op.store_reduced_ensemble_dataset(cycle=cycle, reduction="std")
